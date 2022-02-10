48f2db3a9c0245b1b16939ee6f4fe60bbb67bcff
JuPrvnt <justine.provent90@gmail.com>
2022-02-10 18:28:01 +0100

    canapés affichés

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,6 @@
 // Je récupère l'endroit où je veux afficher mes produits au panier
-let article = document.querySelector("article");
+let article = document.getElementById('cart__items');
+let deleteCart = document.getElementsByClassName("deleteItem");
 // console.log(article);
 
 // Depuis la page Panier, récupérer le panier (l’array) via localStorage
@@ -8,7 +9,7 @@ let productInCart = JSON.parse(localStorage.getItem('canape'));
 
 // Je parcours l’array de mon localStorage
 // Je connecte le site à l'API : si j'ai un résultat correspondant, je retourne le résultat de l'API, sinon, message d'erreur
-
+let canapeAfficher = "";
 
 for (let canape of productInCart) {
   // console.log(canape._id);
@@ -22,9 +23,10 @@ for (let canape of productInCart) {
       }
       throw new Error(res.statusText);
     })
+    // Je créé et insére les éléments du localStorage dans la page Panier
     .then((value) => {
-      // Je créé et insére les éléments du localStorage dans la page Panier
-      const canapeAfficher = `
+      canapeAfficher += `
+                <article class="cart__item" data-id="${value._id}" data-color="${value.colors}">
                 <div class="cart__item__img">
                 <img src="${value.imageUrl}" alt="Photographie d'un canapé">
                 </div>
@@ -44,48 +46,29 @@ for (let canape of productInCart) {
                     </div>
                 </div>
                 </div>
+                </article> 
             `;
       article.insertAdjacentHTML('afterbegin', canapeAfficher);
-
       // console.log(canape);
-    }
-    )
+    })
 }
 
+// J'additionne et j'affiche le total
+let priceProductInCart = [value.price];
+console.log(priceProductInCart);
+let sum = 0;
+for (let t = 0; t < priceProductInCart.length; t++) {
+  sum += priceProductInCart[t];
+}
+console.log(sum);
+
 /*
-<div class="cart__price">
-<p>Total (<span id="totalQuantity"><!-- 2 --></span> articles) : <span id="totalPrice"><!-- 84,00 --></span> €</p>
-</div>
-<div class="cart__order">
-<form method="get" class="cart__order__form">
-  <div class="cart__order__form__question">
-    <label for="firstName">Prénom: </label>
-    <input type="text" name="firstName" id="firstName" required>
-    <p id="firstNameErrorMsg"><!-- ci est un message d'erreur --></p>
-  </div>
-  <div class="cart__order__form__question">
-    <label for="lastName">Nom: </label>
-    <input type="text" name="lastName" id="lastName" required>
-    <p id="lastNameErrorMsg"></p>
-  </div>
-  <div class="cart__order__form__question">
-    <label for="address">Adresse: </label>
-    <input type="text" name="address" id="address" required>
-    <p id="addressErrorMsg"></p>
-  </div>
-  <div class="cart__order__form__question">
-    <label for="city">Ville: </label>
-    <input type="text" name="city" id="city" required>
-    <p id="cityErrorMsg"></p>
-  </div>
-  <div class="cart__order__form__question">
-    <label for="email">Email: </label>
-    <input type="email" name="email" id="email" required>
-    <p id="emailErrorMsg"></p>
-  </div>
-  <div class="cart__order__form__submit">
-    <input type="submit" value="Commander !" id="order">
-  </div>
-</form>
-</div>
+    // Je supprime du panier au click du bouton "supprimer"
+    .then(() => {
+      deleteCart.addEventListener('click', (event) => {
+        event.preventDefault();
+        localStorage.removeItem();
+      })
+    })
 */
+
